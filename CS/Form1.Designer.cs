a2ac3a59d91a9f551ec72d1a7464842fbd8cf9d9
Kseniya Kuzmina <kuzminaksyushka@gmail.com>
2022-08-23 11:23:08 +0400

    add example content

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WinFormsMap {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.mapControl1 = new DevExpress.XtraMap.MapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapControl1
+            // 
+            this.mapControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapControl1.Location = new System.Drawing.Point(12, 12);
+            this.mapControl1.Name = "mapControl1";
+            this.mapControl1.Size = new System.Drawing.Size(996, 381);
+            this.mapControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 427);
+            this.Controls.Add(this.mapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraMap.MapControl mapControl1;
+    }
+}
+
---
+++ CS/Form1.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.XtraMap;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WinFormsMap {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            var layer = new ImageLayer();
+            var provider = new CustomVectorTileStreamProvider();
+
+            layer.DataProvider = provider;
+            mapControl1.Layers.Add(layer);
+        }
+    }
+    public class CustomVectorTileStreamProvider : VectorTileDataProviderBase {
+        public override Stream GetStream(long x, long y, long level) {
+            string path = Path.GetFullPath("..\\..\\Data\\test.data");
+            return File.OpenRead(path);
+        }
+    }
+}
